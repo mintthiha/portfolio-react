f2a17c6b0b00804b7db6b6831e32c6bee60c36b5
mintthiha <mintthiha@gmail.com>
2025-01-20 21:52:05 -0500

    Add Skeleton Code

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>
+          Thiha Min Thein
+        </h1>
+        <h2>
+          <em> Using React + Vite. </em>
+        </h2>
+      </header>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
