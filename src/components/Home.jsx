f70b90b76a6ead62adc20c726d5d25bf3e290b8f
mintthiha <mintthiha@gmail.com>
2025-01-20 22:07:37 -0500

    Fix folder placements

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <h1> Thiha Min Thein </h1>
+      <h3> <em> Using React + Vite </em> </h3>
+      <p> More content will be added here! </p>
+    </>
+  )
+}
+
+export default Home
